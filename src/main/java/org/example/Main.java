8f8ac35c6819879c5bef72aa6db15731d9b67553
RychardSalles <rychardsilva205@gmail.com>
2026-05-19 19:32:57 -0300

    teste do usuario

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,20 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.example.model.Usuario;
+
 public class Main {
-    static void main(String[] args) {
+    public static void main(String[] args) {
 
         Usuario usuario = new Usuario();
 
+        usuario.setId(1);
+        usuario.setNome("Rychard");
+        usuario.setEmail("rychard@email.com");
+        usuario.setSenha("123456");
+
+        System.out.println("ID: " + usuario.getId());
+        System.out.println("Nome: " + usuario.getNome());
+        System.out.println("Email: " + usuario.getEmail());
+        System.out.println("Senha: " + usuario.getSenha());
     }
 }
